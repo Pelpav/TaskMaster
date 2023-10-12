8ea73299a652643915afb0b038b748e6ffce7b16
Pelpav <112887005+Pelpav@users.noreply.github.com>
2023-10-12 22:55:14 +0000

    Delete js directory

--- js/login.js
+++
@@ -1,9 +0,0 @@
-//Ajoute une classe 'glowIcon' à l'élément précédent avec la classe '.fa' lorsqu'un élément avec la classe '.input_text' est en focus
-$(".input_text").focus(function(){
-    $(this).prev('.fa').addclass('glowIcon')
-})
-//Supprime la classe 'glowIcon' de l'élément précédent avec la classe '.fa' lorsqu'un élément avec la classe '.input_text' perd le focus
-$(".input_text").focusout(function(){
-    $(this).prev('.fa').removeclass('glowIcon')
-})
-
